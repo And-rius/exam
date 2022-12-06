4cc3d6c2d8312b949454c67a7c2ca8a7cf95db4b
And-rius <102245341+And-rius@users.noreply.github.com>
2022-12-06 22:37:12 +0200

    sukurti login funkcionaluma

--- base/views.py
+++ base/views.py
@@ -3,9 +3,20 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
 from .models import Task
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
@@ -21,12 +32,10 @@ class TaskCreate(CreateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
-
 
 class DeleteView(DeleteView):
     model = Task
